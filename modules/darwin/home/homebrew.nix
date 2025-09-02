7de35eae4760855df5eb8427ec3ef41238a50eaa
David <superd001@gmail.com>
2025-09-02 11:11:29 +0200

    bump

--- modules/darwin/home/homebrew.nix
+++ modules/darwin/home/homebrew.nix
@@ -46,6 +46,8 @@ _:
 
       "parsec"
       "nordvpn"
+
+      "camo-studio"
     ];
   };
 }
--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -1,13 +1,20 @@
 { pkgs }:
 
 with pkgs;
-let shared-packages = import ../shared/packages.nix { inherit pkgs; }; in
+let
+  shared-packages = import ../shared/packages.nix { inherit pkgs; };
+  ruby_3_2_0 = pkgs.ruby_3_2.overrideAttrs (old: {
+    version = "3.2.0";
+    src = pkgs.fetchurl {
+      url = "https://cache.ruby-lang.org/pub/ruby/3.2/ruby-3.2.0.tar.gz";
+      sha256 = "0wn2mdvvx44hn6vf59pzq0v3l3whmmvvdkpfipwq69qb6vhpians";
+    };
+  });
+in
 shared-packages ++ [
   dockutil
-  # macos dmenu
   raycast
-  # macos i3
   aerospace
   xcodes
-  ruby
+  ruby_3_2_0
 ]
